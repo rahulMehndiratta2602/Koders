f48863b74303733adcb9f332cbf145f7f6acba7e
rahulMehndiratta2602 <f20171479@hyderabad.bits-pilani.ac.in>
2022-03-03 15:45:58 +0530

    Pushed Client Again . First time there was some error

--- middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandlingMiddleware = (err, req, res, next) => {
-    console.log(":boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom::boom:", err)
-    res.status(500).json({ msg: "Internal Server Error" })
-
-}
-
-
-export default errorHandlingMiddleware
